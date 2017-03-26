44cc09376028041fa1dcd343b4cf84769db558ce
Matúš Tomlein <matus.tomlein@gmail.com>
2017-03-26 19:31:39 -0400

    Update dataset URLs

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/ESC-50.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/matus-home.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/movement.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/recordings.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-kitchen-mites-processed.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-kitchen-mites.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-kitchen-mites-features.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-mites-colocated-features.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-mites-colocated-processed.zip
-wget https://dl.dropboxusercontent.com/u/139287/tansfer-learning-datasets/synergy-mites-colocated.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/ESC-50.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/matus-home.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/movement.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/recordings.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-kitchen-mites-features.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-kitchen-mites-processed.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-kitchen-mites.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-mites-colocated-features.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-mites-colocated-processed.zip
+wget https://f001.backblazeb2.com/file/phd-transfer/datasets/synergy-mites-colocated.zip
